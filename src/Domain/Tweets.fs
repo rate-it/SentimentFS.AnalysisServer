b50d7a20be9234762237b96d1b9d292cf8e7475a
Dominik Kotecki <dominikus1910@gmail.com>
2017-08-26 17:50:55 +0200

    MongoDb

--- src/Domain/Tweets.fs
+++ src/Domain/Tweets.fs
@@ -1,5 +1,11 @@
 namespace SentimentFS.AnalysisServer.Domain
 
-module Read =
-
-    type Tweet = {  }
+[<CLIMutable>]
+type TweetDto = {  [<BsonId>]IdStr: string
+                [<BsonElement("text")>] Text: string
+                [<BsonElement("key")>] Key: string
+                [<BsonElement("date")>] Date: DateTime
+                [<BsonElement("lang")>] Lang: string
+                [<BsonElement("longitude")>] Longitude: double
+                [<BsonElement("latitude")>] Latitude: double
+                [<BsonElement("sentiment")>] Sentiment: int }
