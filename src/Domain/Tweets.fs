49ac818bf7f318cd603aecf673f1e915b975059f
Dominik Kotecki <dominikus1910@gmail.com>
2017-08-26 17:56:57 +0200

    add Tweets

--- src/Domain/Tweets.fs
+++ src/Domain/Tweets.fs
@@ -1,11 +1,19 @@
 namespace SentimentFS.AnalysisServer.Domain
+open System
+open MongoDB
+open MongoDB.Bson.Serialization.Attributes
+
+type Sentiment =
+    | Negative = -1
+    | Neutral = 0
+    | Positive = 1
 
 [<CLIMutable>]
-type TweetDto = {  [<BsonId>]IdStr: string
-                [<BsonElement("text")>] Text: string
-                [<BsonElement("key")>] Key: string
-                [<BsonElement("date")>] Date: DateTime
-                [<BsonElement("lang")>] Lang: string
-                [<BsonElement("longitude")>] Longitude: double
-                [<BsonElement("latitude")>] Latitude: double
-                [<BsonElement("sentiment")>] Sentiment: int }
+type Tweet = { [<BsonId>] IdStr: string
+               [<BsonElement("text")>] Text: string
+               [<BsonElement("key")>] Key: string
+               [<BsonElement("date")>] Date: DateTime
+               [<BsonElement("lang")>] Lang: string
+               [<BsonElement("longitude")>] Longitude: double
+               [<BsonElement("latitude")>] Latitude: double
+               [<BsonElement("sentiment")>] Sentiment: Sentiment }
