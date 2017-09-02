0df38a0cd18ef1be7d340839c8149abddbf056d1
Dominik Kotecki <dominikus1910@gmail.com>
2017-09-02 11:26:17 +0200

    small change

--- src/Core/Tweets.fs
+++ src/Core/Tweets.fs
@@ -1,1 +1,20 @@
 namespace SentimentFS.AnalysisServer.Core
+
+module TweetsManager =
+    let spawn =
+        MailboxProcessor.Start(fun agent ->
+            let rec messageLoop() = async{
+
+                // read a message
+                let! msg = agent.Receive()
+
+                // process a message
+                printfn "message is: %s" msg
+
+                // loop to top
+                return! messageLoop()
+                }
+
+    // start the loop
+            messageLoop()
+        )
