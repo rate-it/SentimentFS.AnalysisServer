b9437322fac5b80834e11f0fa7b3499be2009725
Dominik Kotecki <dominikus1910@gmail.com>
2017-09-04 20:56:44 +0200

    add mongo tweets operations

--- src/Core/Tweets.fs
+++ src/Core/Tweets.fs
@@ -1,20 +1,41 @@
 namespace SentimentFS.AnalysisServer.Core
 
-module TweetsManager =
-    let spawn() =
+module TweetsStorage =
+    open SentimentFS.AnalysisServer.Domain.Tweets
+    open MongoDB.Driver
+    open MongoDB.Bson
+    open MongoDB.Driver
+    open MongoDB.Driver.Linq
+
+    let private TweetsCollection (db:IMongoDatabase) = db.GetCollection<Tweet>("tweets")
+
+    let private store (tweets: Tweets) (col: IMongoCollection<Tweet>) =
+        async {
+            return! col.InsertManyAsync(tweets.value) |> Async.AwaitTask
+        }
+
+    let private getByKey (key:string) (col: IMongoCollection<Tweet>) =
+        async {
+            let! result = col.Find(fun tweet -> tweet.Key = key).ToListAsync() |> Async.AwaitTask
+            return match result |> Seq.toList with
+                   | [] -> None
+                   | l -> Some { value = l }
+        }
+
+
+    let spawn(db: IMongoDatabase) =
         MailboxProcessor.Start(fun agent ->
-            let rec messageLoop() = async {
-
-                // read a message
+            let rec loop() = async {
                 let! msg = agent.Receive()
+                match msg with
+                | Store tweets ->
+                    do! db |> TweetsCollection |> store(tweets)
+                    return! loop()
+                | GetByKey(key, reply) ->
+                    let! res = db |> TweetsCollection |> getByKey key
+                    reply.Reply(res)
+                    return! loop()
 
-                // process a message
-                printfn "message is: %s" msg
-
-                // loop to top
-                return! messageLoop()
             }
-
-    // start the loop
-            messageLoop()
+            loop()
         )
--- src/Domain/Analysis.fs
+++ src/Domain/Analysis.fs
@@ -9,11 +9,17 @@ type Trend =
     | Decreasing = -1
 
 type AnalysisScore = { SentimentByQuantity: IDictionary<Sentiment, int>
-                       KeyWords: struct (string * int)
-                       Localizations: struct (float32 * float32)
+                       KeyWords: struct (string * int) seq
+                       Localizations: struct (float32 * float32) seq
                        Key: string
                        Trend: Trend
                        DateByQuantity: IDictionary<DateTime, int> }
+    with static member Zero key = { SentimentByQuantity = Dictionary<Sentiment, int>()
+                                    KeyWords = [||]
+                                    Localizations = [||]
+                                    Key = key
+                                    Trend = Trend.Stable
+                                    DateByQuantity = Dictionary<DateTime, int>() }
 
 type Query =
     | GetAnalysisForKey of key:string
