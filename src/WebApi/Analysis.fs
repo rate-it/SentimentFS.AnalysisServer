65d3d4d5adfd91d8b5d1e7eb023b80262dc6d793
Dominik Kotecki <dominikus1910@gmail.com>
2017-09-10 12:03:49 +0200

    async

--- src/WebApi/Analysis.fs
+++ src/WebApi/Analysis.fs
@@ -1,12 +1,28 @@
 namespace SentimentFS.AnalysisServer.WebApi
 
 module Analysis =
+    open Akka.Actor
     open Suave
     open Suave.Filters
     open Suave.Operators
     open Suave.Successful
+    open SentimentFS.AnalysisServer.Core.Analysis
+    open SentimentFS.AnalysisServer.Domain.Analysis
 
     let analysisController() =
+        let actorSystem =
+            ActorSystem.Create("sentimentfs")
+
+        let analysisActor =
+            actorSystem.ActorOf(Props.Create<AnalysisActor>())
+
+        let getAnalysisResultByKey(key):WebPart =
+            fun (x : HttpContext) ->
+                async {
+                    let result = ""
+                    return! OK key x
+                }
+
         pathStarts "/api/analysis" >=> choose [
-            GET >=> choose [ pathScan "/api/analysis/result/%s"  (fun key -> OK(key)) ]
+            GET >=> choose [ pathScan "/api/analysis/result/%s" getAnalysisResultByKey ]
         ]
