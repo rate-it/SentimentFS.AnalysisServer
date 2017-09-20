6cd95b0cfaa23d8a967aee651b177e971e78e6f6
Dominik Kotecki <dominikus1910@gmail.com>
2017-09-20 20:21:48 +0200

    remove bson annotations

--- src/Domain/Tweets.fs
+++ src/Domain/Tweets.fs
@@ -1,18 +1,16 @@
 namespace SentimentFS.AnalysisServer.Domain.Tweets
 open System
-open MongoDB
-open MongoDB.Bson.Serialization.Attributes
 open SentimentFS.AnalysisServer.Domain.Sentiment
 
 [<CLIMutable>]
-type Tweet = { [<BsonId>] IdStr: string
-               [<BsonElement("text")>] Text: string
-               [<BsonElement("key")>] Key: string
-               [<BsonElement("date")>] Date: DateTime
-               [<BsonElement("lang")>] Lang: string
-               [<BsonElement("longitude")>] Longitude: double
-               [<BsonElement("latitude")>] Latitude: double
-               [<BsonElement("sentiment")>] Sentiment: Sentiment }
+type Tweet = { IdStr: string
+               Text: string
+               Key: string
+               Date: DateTime
+               Lang: string
+               Longitude: double
+               Latitude: double
+               Sentiment: Sentiment }
 
 
 type Tweets = { value: Tweet list }
