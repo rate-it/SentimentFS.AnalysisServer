091b5cd24949a4a2a3da912471f36c5059645d96
Dominik Kotecki <dominikus1910@gmail.com>
2017-10-03 21:27:23 +0200

    cedentials

--- src/WebApi/Analysis.fs
+++ src/WebApi/Analysis.fs
@@ -7,13 +7,27 @@ module Analysis =
     open Suave.Operators
     open Suave.Successful
     open SentimentFS.AnalysisServer.Core.Analysis
+    open Cassandra
+    open Tweetinvi
 
     let analysisController() =
         let actorSystem =
             ActorSystem.Create("sentimentfs")
 
+        let cluster =
+            Cluster
+                .Builder()
+                .AddContactPoint("127.0.0.1")
+                .WithPort(8881)
+                .WithDefaultKeyspace("sentiment_fs")
+                .Build()
+
+        let session = cluster.ConnectAndCreateDefaultKeyspaceIfNotExists()
+
         let analysisActor =
             actorSystem.ActorOf(Props.Create<AnalysisActor>())
+
+        let credientials = Auth.SetUserCredentials("", "", "", "")
 
         let getAnalysisResultByKey(key):WebPart =
             fun (x : HttpContext) ->
