def0f05716ec90ce95e8bdcc7871021f5471b258
Dominik Kotecki <dominikus1910@gmail.com>
2017-10-04 20:09:08 +0200

    snetimnet

--- src/WebApi/Analysis.fs
+++ src/WebApi/Analysis.fs
@@ -11,19 +11,46 @@ module Analysis =
     open SentimentFS.AnalysisServer.Core.Actor
     open Cassandra
     open Tweetinvi
+    open System.Net.Http
+    open Newtonsoft.Json
+
+    let intToEmotion (value: int): Emotion =
+        match value with
+        | -5 | -4 -> Emotion.VeryNegative
+        | -3 | -2 | -1 -> Emotion.Negative
+        | 0 -> Emotion.Neutral
+        | 1 | 2 | 3 -> Emotion.Positive
+        | 4 | 5 -> Emotion.VeryPositive
+        | _ -> Emotion.Neutral
+
+    let initSentimentActor (trainDataUrl: string) (sentimentActor: IActorRef) =
+        let httpResult = async {
+            use client = new HttpClient()
+            let! result = client.GetAsync(System.Uri(trainDataUrl)) |> Async.AwaitTask
+            result.EnsureSuccessStatusCode() |> ignore
+            return! result.Content.ReadAsStringAsync() |> Async.AwaitTask } |> Async.RunSynchronously
+
+        let emotions = httpResult
+                            |> JsonConvert.DeserializeObject<Map<string, int>>
+                            |> Map.toList
+                            |> List.map(fun (word, em) -> struct (word, em |> intToEmotion))
+
+        for struct (word, emotion) in emotions do
+            sentimentActor.Tell({ trainQuery =  { value = word; category = emotion; weight = None } })
+
 
     let analysisController() =
         let actorSystem =
             ActorSystem.Create("sentimentfs")
 
-        let cluster =
-            Cluster
-                .Builder()
-                .AddContactPoint("127.0.0.1")
-                .WithDefaultKeyspace("sentiment_fs")
-                .Build()
+        // let cluster =
+        //     Cluster
+        //         .Builder()
+        //         .AddContactPoint("127.0.0.1")
+        //         .WithDefaultKeyspace("sentiment_fs")
+        //         .Build()
 
-        let session = cluster.ConnectAndCreateDefaultKeyspaceIfNotExists()
+        // let session = cluster.ConnectAndCreateDefaultKeyspaceIfNotExists()
 
         let analysisActor =
             actorSystem.ActorOf(Props.Create<AnalysisActor>(), Actors.analysisActor.Name)
--- src/WebApi/Program.fs
+++ src/WebApi/Program.fs
@@ -2,9 +2,20 @@
 
 open System
 open Suave
+open Akka.Actor
 open SentimentFS.AnalysisServer
+open SentimentFS.AnalysisServer.WebApi.Analysis
+open SentimentFS.AnalysisServer.Core.Sentiment
+open SentimentFS.AnalysisServer.Core.Actor
 
 module Program =
+    open SentimentFS.NaiveBayes.Dto
+
+    let actorSystem =
+            ActorSystem.Create("sentimentfs")
+
+    let sentimentActor =
+            actorSystem.ActorOf(Props.Create<SentimentActor>(Some defaultClassificatorConfig), Actors.sentimentActor.Name)
 
     let GetEnvVar var =
         match System.Environment.GetEnvironmentVariable(var) with
@@ -18,13 +29,16 @@ module Program =
 
     [<EntryPoint>]
     let main argv =
-        try
-            WebServer.start (getPortsOrDefault 8080us)
-            0 // return an integer exit code
-        with
-        | ex ->
-            let color = System.Console.ForegroundColor
-            System.Console.ForegroundColor <- System.ConsoleColor.Red
-            System.Console.WriteLine(ex.Message)
-            System.Console.ForegroundColor <- color
-            1
+        // try
+        //     WebServer.start (getPortsOrDefault 8080us)
+        //     0 // return an integer exit code
+        // with
+        // | ex ->
+        //     let color = System.Console.ForegroundColor
+        //     System.Console.ForegroundColor <- System.ConsoleColor.Red
+        //     System.Console.WriteLine(ex.Message)
+        //     System.Console.ForegroundColor <- color
+        //     1
+        (initSentimentActor("https://raw.githubusercontent.com/wooorm/afinn-96/master/index.json") sentimentActor)
+        printfn "%A" (sentimentActor.Ask<ClassificationScore<Emotion>>({ text = "My brother hate java" }) |> Async.AwaitTask |> Async.RunSynchronously)
+        0
