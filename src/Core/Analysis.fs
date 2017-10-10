f7c90a6a51263ffa4fd2361a2902846ee2a84fe1
Dominik Kotecki <dominikus1910@gmail.com>
2017-10-10 22:04:50 +0200

    Trend

--- src/Core/Analysis.fs
+++ src/Core/Analysis.fs
@@ -26,6 +26,36 @@ type AnalysisScore = { SentimentByQuantity: IDictionary<Emotion, int>
 
 type GetAnalysisForKey = { key : string }
 
+module Trend =
+
+    let private average(nums: float list) =
+        let length = nums |> List.length |> double
+        let sum = nums |> List.sum |> double
+        sum / length
+
+    let private a(nums: float list) =
+        let y = nums
+        let t = [0.0..(y |> List.length |> double)]
+        let averageT = t |> average
+        let averageY = y |> average
+        let tmta = t |> List.map(fun x -> x - averageT)
+        let ymya = y |> List.map(fun x -> x - averageY)
+        let numerator  = tmta |> List.zip ymya |> List.map (fun (a, b) -> a * b) |> List.fold ((+)) 0.0
+        let denominator = tmta |> List.map(fun x -> x ** 2.0) |> List.fold((+)) 0.0
+        numerator / denominator
+
+    let rate (nums: int seq): Trend =
+        let list = nums |> Seq.toList |> List.map(double)
+        match list with
+        | [] -> Trend.Stable
+        | list ->
+            let a = list |> a
+            if Math.Abs(a) < Double.Epsilon then
+                Trend.Stable
+            else
+                if a > 0.0 then Trend.Increasing else Trend.Decreasing
+
+
 
 type AnalysisActor() as this =
     inherit ReceiveActor()
