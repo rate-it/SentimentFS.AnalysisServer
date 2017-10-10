2483d738fe00bf3f6c003b64a60639fa86d2f2e5
Dominik Kotecki <dominikus1910@gmail.com>
2017-10-10 21:34:46 +0200

    watch

---
+++ tests/Core.Tests/Analysis.Tests.fs
@@ -0,0 +1,14 @@
+namespace SentimentFS.AnalysisServer.Core.Tests
+
+module Analysis =
+    open Expecto
+    open SentimentFS.AnalysisServer.Core.Analysis
+
+
+    [<Tests>]
+    let tests =
+        testList "Analysis" [
+            testList "Trend" [
+
+            ]
+        ]
