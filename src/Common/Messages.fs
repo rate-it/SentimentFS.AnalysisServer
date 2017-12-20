b509ae43dd179476bf27a3b602ad81f541036a1e
Dominik Kotecki <dominikus1910@gmail.com>
2017-12-20 21:18:12 +0100

    add Sentiment messages

--- src/Common/Messages.fs
+++ src/Common/Messages.fs
@@ -1,1 +1,28 @@
 namespace SentimentFS.AnalysisServer.Messages
+
+
+module Sentiment =
+
+    type Emotion =
+        | VeryNegative = -2
+        | Negative = -1
+        | Neutral = 0
+        | Positive = 1
+        | VeryPositive = 2
+
+    [<CLIMutable>]
+    type Classify = { text : string }
+
+    [<CLIMutable>]
+    type Train = { value: string; category: Emotion; weight : int option }
+
+    type SentimentActorCommand =
+        | Train of Train
+        | Classify of Classify
+
+    type SentimentMessage =
+        | TrainEvent of Train
+        | SentimentCommand of SentimentActorCommand
+
+    [<CLIMutable>]
+    type ClassifyResult = { text: string; score: Map<Emotion, float> }
