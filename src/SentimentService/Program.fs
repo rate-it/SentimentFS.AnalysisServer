f0614011e5a1d79c6a0e68ea7ad54948ea44955d
Dominik Kotecki <dominikus1910@gmail.com>
2017-12-21 20:56:38 +0100

    fix tests

--- src/SentimentService/Program.fs
+++ src/SentimentService/Program.fs
@@ -15,9 +15,5 @@ module Program =
         let system = System.create "sentimentfs" <| Configuration.load()
         let remoteProps addr actor = { propsPersist actor with Deploy = Some (Deploy(RemoteScope(Address.Parse addr)));}
         let actor = spawn system "sentiment" <| (remoteProps "akka.tcp://sentimentfs@localhost:4500" (sentimentActor(Some defaultClassificatorConfig)))
-        // actor <! SentimentCommand(Train({ trainQuery = { value = "I love fsharp"; category = Emotion.Positive; weight = None } }))
-        // actor <! SentimentCommand(Train({ trainQuery = { value = "I hate java"; category = Emotion.Negative; weight = None } }))
-        // async { let! reply = actor <? SentimentCommand(Classify({ text = "My brother love fsharp" }))
-        //         printfn "Current state of %A: %A" actor reply } |> Async.RunSynchronously
         Console.ReadKey();
         0 // return an integer exit code
