b77218335dc99e00be55987e912f910e74799936
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2017-12-22 13:45:00 +0100

    small changes

--- src/SentimentService/Program.fs
+++ src/SentimentService/Program.fs
@@ -6,6 +6,7 @@ open Actor
 open SentimentFS.NaiveBayes.Dto
 open Akkling
 open System
+open Akkling.Cluster.Sharding
 
 module Program =
     open Akka.Actor
@@ -13,7 +14,6 @@ module Program =
     [<EntryPoint>]
     let main argv =
         let system = System.create "sentimentfs" <| Configuration.load()
-        let remoteProps addr actor = { propsPersist actor with Deploy = Some (Deploy(RemoteScope(Address.Parse addr)));}
-        let actor = spawn system "sentiment" <| (remoteProps "akka.tcp://sentimentfs@localhost:5002" (sentimentActor(Some defaultClassificatorConfig)))
+        let actor = ClusterSharding.entityFactoryFor system "sentiment" <| propsPersist (sentimentActor(Some defaultClassificatorConfig))
         Console.ReadKey() |> ignore
         0 // return an integer exit code
