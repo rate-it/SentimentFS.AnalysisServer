3d5b1224ad8a093a143bcd5dc937f56ec579b4a3
Dominik Kotecki <dominikus1910@gmail.com>
2017-12-23 16:23:30 +0100

    small change

--- src/SentimentService/Program.fs
+++ src/SentimentService/Program.fs
@@ -7,14 +7,15 @@ open SentimentFS.NaiveBayes.Dto
 open Akkling
 open System
 open Akkling.Cluster.Sharding
+open Akka.Cluster.Tools.Singleton
 
 module Program =
     open Akka.Actor
 
     [<EntryPoint>]
     let main argv =
-        let system = System.create "sentimentfs" <| Configuration.load()
-        let actor = ClusterSharding.entityFactoryFor system "sentiment" <| propsPersist (sentimentActor(Some defaultClassificatorConfig))
+        let system = System.create "sentimentfs" <| (Configuration.load().WithFallback(ClusterSingletonManager.DefaultConfig()))
+        let actor = spawn system "classifier" <| propsPersist (sentimentActor(Some defaultClassificatorConfig))
         printfn "cluster start"
         Console.ReadKey() |> ignore
         0 // return an integer exit code
