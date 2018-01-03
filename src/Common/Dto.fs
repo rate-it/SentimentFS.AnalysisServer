86058d312b33508c4941d2d523beb78fe97f71cd
Dominik Kotecki <dominikus1910@gmail.com>
2018-01-03 20:10:09 +0100

    add TwitterCredentials dto

---
+++ src/Common/Dto.fs
@@ -0,0 +1,5 @@
+namespace SentimentFS.AnalysisServer.Common.Dto
+
+module TwitterApi =
+
+    type TwitterCredentials = { ConsumerKey: string; ConsumerSecret: string; AccessToken: string; AccessTokenSecret: string }
