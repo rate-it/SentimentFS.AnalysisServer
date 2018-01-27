91955476beda7547f5eb630adc409f887e3b57f0
Dominik Kotecki <dominikus1910@gmail.com>
2018-01-27 19:09:22 +0100

    small change

--- src/TweetsService/Database.fs
+++ src/TweetsService/Database.fs
@@ -1,5 +1,19 @@
 namespace SentimentFS.AnalysisServer
 
-module Cassadra =
+module CassandraDb =
+    open Cassandra
 
-    let a = 2
+    let private createTweetsCollectionIfNotExists (session: ISession) =
+        session.Execute("""
+                          CREATE TABLE IF NOT EXISTS tweets (
+                            id_str varchar,
+                            text text,
+                            key varchar,
+                            date timestamp,
+                            lang varchar,
+                            longitude double,
+                            latitude double,
+                            sentiment int,
+                            PRIMARY KEY(key, id_str)
+                          );
+                        """)
