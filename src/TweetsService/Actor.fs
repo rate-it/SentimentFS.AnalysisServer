716c6900359e43b74f472fd7a37e577104a3456c
Dominik Kotecki <dominikus1910@gmail.com>
2018-01-29 20:04:04 +0100

    small chanegs

--- src/TweetsService/Actor.fs
+++ src/TweetsService/Actor.fs
@@ -2,7 +2,6 @@ namespace SentimentFS.AnalysisServer
 
 open Common.Messages.Twitter
 open Akka.Streams.Dsl
-open Common.Dto.TwitterApi
 open Akkling.Streams
 open Akka
 open Tweetinvi.Parameters
@@ -12,10 +11,20 @@ open Tweetinvi
 open SentimentFS.AnalysisServer.Common.Messages.Sentiment
 open Akkling
 open Akka.Streams.Dsl
-open Akka.Streams.Dsl
 open Akka.Streams
 
 module TwitterApi =
+
+    let downloadTweetsFromApi q =
+        async {
+            let options = SearchTweetsParameters(q.key)
+            options.SearchType <- Nullable<SearchResultType>(SearchResultType.Recent)
+            options.Lang <- Nullable<LanguageFilter>(LanguageFilter.English)
+            options.Filters <- TweetSearchFilters.None
+            options.MaximumNumberOfResults <- q.quantity
+            options.Since <- q.since
+            return! SearchAsync.SearchTweets(options) |> Async.AwaitTask
+        }
 
     let downloadTweetsFlow (maxConcurrentDownloads: int)(credentials: TwitterCredentials) =
         Flow.id
@@ -69,7 +78,7 @@ module TwitterApi =
 
 module Actor =
 
-    let tweetsActor (mailbox: Actor<TweetsMessage>) =
+    let tweets (mailbox: Actor<TweetsMessage>) =
         let rec loop (state) =
             actor {
                 let! msg = mailbox.Receive()
