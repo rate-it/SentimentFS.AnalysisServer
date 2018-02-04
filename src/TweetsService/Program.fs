1c6e7f5402a3014f79d42a9dced7a7073abf7f26
Dominik Kotecki <dominikus1910@gmail.com>
2018-02-04 17:18:56 +0100

    actor

--- src/TweetsService/Program.fs
+++ src/TweetsService/Program.fs
@@ -1,10 +1,15 @@
 ﻿// Learn more about F# at http://fsharp.org
 namespace  SentimentFS.AnalysisServer.ActorService
+open SentimentFS.AnalysisServer.Actor
+open SentimentFS.AnalysisServer
 
 module Program =
-    open System
+    open Akkling
 
     [<EntryPoint>]
     let main argv =
+        let system = System.create "sentimentfs" <| (Configuration.load())
+        let actorProps = tweetsActor((Storage.get InMemory))({ credentials = null; sentimentActorPath = "" })
+        let actor = spawn system "classifier" <| props (actorProps)
         printfn "Hello World from F#!"
         0 // return an integer exit code
