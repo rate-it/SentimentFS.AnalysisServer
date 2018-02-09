20ff05739eb32a4dae945a4137d345d859499d07
Dominik Kotecki <dominikus1910@gmail.com>
2018-02-09 20:10:02 +0100

    remove cassandra code

--- src/TweetsService/Database.fs
+++ src/TweetsService/Database.fs
@@ -1,7 +1,6 @@
 namespace SentimentFS.AnalysisServer
 open System
 open SentimentFS.AnalysisServer.Common.Messages.Sentiment
-open Cassandra
 open SentimentFS.AnalysisServer.Common.Messages.Twitter
 open Nest
 
@@ -16,14 +15,6 @@ module Dto =
                       Longitude: double
                       Latitude: double
                       Sentiment: Emotion } with
-        static member FromCassandraRow(x: Row) = { IdStr = x.GetValue<string>("id_str")
-                                                   Text = x.GetValue<string>("text")
-                                                   HashTags = x.GetValue<string seq>("key")
-                                                   Date = x.GetValue<DateTime>("creation_date")
-                                                   Lang = x.GetValue<string>("lang")
-                                                   Longitude = x.GetValue<double>("longitude")
-                                                   Latitude = x.GetValue<double>("latitude")
-                                                   Sentiment = (enum<Emotion>(x.GetValue<int>("sentiment"))) }
         static member FromTweet(x: Tweet) =
             { IdStr = x.IdStr
               HashTags = x.HashTags
@@ -52,37 +43,6 @@ module Dto =
                                   Latitude = 0.0
                                   Sentiment = Emotion.Neutral }
 open Dto
-
-
-module CassandraDb =
-    let createTweetsCollectionIfNotExists (session: ISession) =
-        session.Execute("""
-                          CREATE TABLE IF NOT EXISTS tweets (
-                            id_str varchar,
-                            text text,
-                            hashtags list<text>,
-                            creation_date timestamp,
-                            lang varchar,
-                            longitude double,
-                            latitude double,
-                            sentiment int,
-                            PRIMARY KEY(id_str)
-                          );
-                        """)
-
-    let store (tweets: Tweets) (session: ISession) =
-        async {
-            let batch = BatchStatement()
-            let query = session.Prepare("""
-                            INSERT INTO tweets (id_str, text, hashtags, creation_date, lang, longitude, latitude, sentiment) VALUES (?, ?, ?, ?, ?, ?, ?, ?);
-                        """)
-            for tweet in tweets.tweets do
-                let coordinates = defaultArg tweet.Coordinates { Longitude = 0.0; Latitude = 0.0 }
-                let emotion = defaultArg tweet.Sentiment Emotion.Neutral |> int
-                query.Bind(tweet.IdStr, tweet.Text, tweet.HashTags, tweet.CreationDate, tweet.Language, coordinates.Longitude, coordinates.Latitude, emotion) |> batch.Add |> ignore
-
-            return! batch |> session.ExecuteAsync |> Async.AwaitTask
-        }
 
 module Elastic =
     open Nest
