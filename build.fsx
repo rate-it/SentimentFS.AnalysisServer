e480c9f26bd8bfbe49ac2d4bdd579bd0869136f6
Dominik Kotecki <dominikus1910@gmail.com>
2018-02-09 21:29:15 +0100

    install fake modules

--- build.fsx
+++ build.fsx
@@ -1,12 +1,16 @@
 #r "paket:
-nuget Fake.Core.Target prerelease"
+nuget Fake.Core.Target prerelease
+nuget Fake.IO.FileSystem
+nuget Fake.DotNet.MsBuild
+nuget Fake.Core.Globbing
+nuget Fake.Core.Target"
 #load "./.fake/build.fsx/intellisense.fsx"
 
 open Fake.Core
-
+open Fake.IO
 // *** Define Targets ***
 Target.Create "Clean" (fun _ ->
-  Trace.log " --- Cleaning stuff --- "
+    Trace.log " --- Building the app --- "
 )
 
 Target.Create "Build" (fun _ ->
